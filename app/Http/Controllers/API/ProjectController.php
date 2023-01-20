571e17d1c6d76edaaf80c8fe11f3d6aaced8e1d2
Marco Inchingoli <evilmarkus@hotmail.it>
2023-01-20 18:42:23 +0100

    Add route for single project, add show method API

--- app/Http/Controllers/API/ProjectController.php
+++ app/Http/Controllers/API/ProjectController.php
@@ -15,4 +15,21 @@ class ProjectController extends Controller
             'results' => Project::with(['type', 'technologies'])->orderByDesc('id')->paginate(6)
         ]);
     }
+
+    public function show($id)
+    {
+        $project = Project::with('type', 'technologies')->where('id', $id)->first();
+        //dd($project);
+        if ($project) {
+            return response()->json([
+                'success' => true,
+                'results' => $project
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'results' => 'Project Not Found'
+            ]);
+        }
+    }
 }
